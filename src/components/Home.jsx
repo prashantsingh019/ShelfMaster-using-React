339b9b9647e1ccdbd053daf8b2242b388e37ea00
prashantsingh019 <ps731278@gmail.com>
2024-12-03 14:58:48 +0530

    corrected some code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import Bookcard from "./Bookcard.JSX";
-import { useState } from "react";
+import { useState,useState } from "react";
 import { useSelector } from "react-redux";
 
 function Home({ searchText, data }) {
+  
   const books = useSelector((state) => state.books.book);
 
   return (
     <main className="book-section flex flex-wrap gap-x-3 gap-y-1 justify-center pt-2">
       {books.map((book) => {
-        return <Bookcard data={book} id={book["id"]}/>
+        return <Bookcard data={book} key={book["id"]}/>
       })}
     </main>
   );
