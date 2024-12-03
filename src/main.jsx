6201e6376ed5317216d179bb2908dc8f23257cba
prashantsingh019 <ps731278@gmail.com>
2024-12-03 14:56:32 +0530

    applied routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
+import { createBrowserRouter,RouterProvider } from 'react-router';
 import App from './App.jsx'
+import Home from './components/Home.jsx';
+import BrowseBook from './components/BrowseBook.jsx';
+import ErrorPage from './components/ErrorPage.jsx';
+const routes = createBrowserRouter([
+  {
+    path:'',
+    element:<App/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/browse+book+page',
+        element:<BrowseBook/>
+      },
+      
+    ],
+    errorElement:<ErrorPage/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
