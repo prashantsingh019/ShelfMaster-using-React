ce3c0bcf4ff8e79e3e7ed2b7ee6d06aaeced415d
prashantsingh019 <ps731278@gmail.com>
2024-12-03 14:58:03 +0530

    browse-book-page

--- src/components/BrowseBook.jsx
+++ src/components/BrowseBook.jsx
@@ -1,7 +1,44 @@
+import { books_data } from "../utils/bookList";
+import Bookcard from "./Bookcard";
+import { useState } from "react";
 const BrowseBook = () => {
+  const [search, setSearch] = useState(" ");
+  const [filtered, setFiltered] = useState(books_data);
+
+  const handleClick = () => {
+    const searchFilter = filtered.filter((book) => {
+      return (
+        book.author.toLowerCase().includes(search.toLowerCase()) ||
+        book.title.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+
+    setFiltered(searchFilter);
+    setSearch(e.target.value == '');
+  };
+
   return (
-    <div className="bg-red-400">
-       
+    <div className="flex-1">
+      <h1 className="text-center text-5xl text-bold">Browse Book Page</h1>
+      <div className="bg-red-400 flex justify-center mt-5 gap-2 p-2">
+        <input
+          type="text"
+          placeholder="search books here"
+          className="p-2 rounded w-80 outline-none"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          className="bg-gray-500 py-1 px-3 rounded-md text-white"
+          onClick={handleClick}
+        >
+          Search
+        </button>
+      </div>
+      <main className="flex justify-center flex-wrap gap-2">
+        {filtered.map((book) => {
+          return <Bookcard data={book} />;
+        })}
+      </main>
     </div>
   );
 };
