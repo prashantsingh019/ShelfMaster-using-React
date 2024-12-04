97a29ad925e9a6b6c1cc4d3d68d2e9fb31eba96e
prashantsingh019 <ps731278@gmail.com>
2024-12-04 09:41:14 +0530

    feature develop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import Bookcard from "./Bookcard.JSX";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { useParams } from "react-router";
 
 function Home({ searchText, data }) {
-  
+  const params = useParams();
+  console.log(params);
+    
   const books = useSelector((state) => state.books.book);
-
+  const category = books.filter((book) => book.id)
   return (
     <main className="book-section flex flex-wrap gap-x-3 gap-y-1 justify-center pt-2">
       {books.map((book) => {
