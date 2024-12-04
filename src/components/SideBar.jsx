17b40c48ca0545fa42dcfd38aaffd04182e249e7
prashantsingh019 <ps731278@gmail.com>
2024-12-04 09:41:50 +0530

    sequence change

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,30 +1,27 @@
 import React from "react";
-import {category} from '../utils/bookList';
+import { category } from "../utils/bookList";
 import { filterBook } from "../redux/bookSlice";
 import { useSelector } from "react-redux";
-import { useState } from "react"; 
+import { useState } from "react";
+import { Link } from "react-router";
+
 function SideBar() {
   const data = useSelector((state) => state.books.book);
-  const [select,setCategory] = useState(" ");
-  console.log(select);
-  
- data.map((book)=>{
-    console.log(book["category"]);
-  })
-  const handleLiClick = (value) => {
-      setCategory(value.toLowerCase());
-  }
-  const filter = data.filter((book) => book["category"] == "Fairy Tales");
-  console.log(typeof filter);
+
   return (
     <aside className="bg-violet-400 pt-2 w-[200px]">
       <h2>Categories</h2>
-      
-      <ul className="nested-ul">
-        {category.map((listItem,index) => {
-           return <li key={index} onClick={()=>handleLiClick(listItem)}>{listItem}</li>
-        })}
 
+      <ul className="nested-ul">
+        {category.map((listItem, index) => {
+          return (
+            <Link to={`${listItem.toLowerCase()}`}>
+              <li key={index} onClick={() => handleLiClick(listItem)}>
+                {listItem}
+              </li>
+            </Link>
+          );
+        })}
       </ul>
     </aside>
   );
